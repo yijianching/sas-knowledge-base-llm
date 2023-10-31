c2696cc24456fe1ef74f52ed81f21c9a19497a10
yijianching <jupyter-azureuser@anemoi3.qj5bpctr5auufbqbj5zzaeai4h.ix.internal.cloudapp.net>
2023-10-31 02:31:15 +0000

    latest

--- streamlit_llama_index.py
+++ streamlit_llama_index.py
@@ -16,31 +16,42 @@ from llama_index.llms import HuggingFaceLLM
 from llama_index import LangchainEmbedding, VectorStoreIndex, ServiceContext
 from llama_index import SimpleDirectoryReader
 
+from llmlingua import PromptCompressor
+
 st.title('【ＳＡＳ】 Quickstart App')
 
 MODEL = "meta-llama/Llama-2-7b-chat-hf"
 
-# system_prompt = """
-# You are an assistant and your job is to only answer questions about SAS Viya 4, based on the given source documents. Here are some rules you must always follow:
-# - Assume if the question is asking about SAS Viya, it is referring to SAS Viya 4.
-# - If a question is not related to SAS Viya 4, respond that you only answer questions related to SAS Viya 4.
-# - Keep your answers based on facts, be as descriptive as possible when describing SAS Viya functionality and do not hallucinate features.
-# - When answering questions, please use dot points and bulleted lists in your answer when detailing a list of instructions.
-# - Never say thank you or that you are an AI agent, just answer the question directly.
-# - Stop being polite and just be direct.
-# """
-system_prompt = """"""
+system_prompt = """
+You are an answer bot on SAS Viya 4 and Kubernetes and your job is to only answer questions about SAS Viya 4 and Kubernetes, based on the given source documents. Here are some rules you must always follow:
+- Assume if the question is asking about SAS Viya, it is referring to SAS Viya 4.
+- If a question is not related to SAS Viya 4, respond that you only answer questions related to SAS Viya 4.
+- Keep your answers based on facts, and be as specific as possible when describing SAS Viya functionality.
+- When answering questions, please use dot points and bulleted lists in your answer.
+- Plz prvid spcfc sggstns fr performance and tuning
+- Do not suggest any information unless there is a source for your information.
+"""
+# - You must provide a source for your information in your response with a specific page number.
+# system_prompt = """"""
 # This will wrap the default prompts that are internal to llama-index
 # query_wrapper_prompt = SimpleInputPrompt("<|USER|>{query_str}<|ASSISTANT|>")
+prompt_template_str = "[INST]<<SYS>>\n" + system_prompt + "<</SYS>>\n\n{query_str}[/INST]"
 query_wrapper_prompt = PromptTemplate(
-    "[INST]<<SYS>>\n" + system_prompt + "<</SYS>>\n\n{query_str}[/INST] "
+    prompt_template_str
 )
+
+## works but slows down. takes way too long. put into another script to look at the prompt compression
+## i think this thing downloads its LLM again because we download checkpoint shards again
+# llm_lingua = PromptCompressor()
+# compressed_prompt = llm_lingua.compress_prompt(prompt_template_str, instruction="", question="", target_token=200)
+# print(compressed_prompt['compressed_prompt'])
+# query_wrapper_prompt = PromptTemplate(compressed_prompt['compressed_prompt'])
 
 @st.cache_resource
 def load_documents2():
     return SimpleDirectoryReader(
-        input_dir="doc_data/", 
-        exclude=["*.md"]
+        input_dir="rfp_data/", 
+        # exclude=["*.md", "*.txt"]
         ).load_data()
 
 @st.cache_resource
@@ -82,7 +93,7 @@ def generate_response2(input_text):
     return st.info(chat_engine.chat(input_text)) #st.info(query_engine.query(input_text)) 
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are some common customizations I can add to my SAS Viya deployment?')
+    text = st.text_area('Enter text:', 'Ask me anything about SAS Viya!')
     submitted = st.form_submit_button('Submit')
     if submitted:
         print("Response submitted")
